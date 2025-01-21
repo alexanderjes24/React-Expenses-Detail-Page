6eed0adfc05da416cf6c3903db50458cdfed7cc2
alexanderjes24 <103304222+alexanderjes24@users.noreply.github.com>
2025-01-21 18:20:37 +0800

    ,

---
+++ Expenses Detail/src/Register.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+import React from 'react'
+
+const Register = () => {
+    const[name,setName] = useState('')
+    
+    return (
+
+        <>
+        <div>Register</div>
+        <form>
+            <div className="form-group">
+                <label htmlFor="NameInput">Name</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div className="form-group">
+                <label htmlFor="exampleInputPassword1">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+            </div>
+            <div className="form-group form-check">
+                <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+    </>
+  )
+}
+
+export default Register
